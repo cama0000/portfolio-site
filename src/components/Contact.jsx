0876deed695e080227e7ec7658052a943730f1fc
cama0000 <acama0000@gmail.com>
2024-07-23 18:08:37 -0500

    add fade animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,23 +6,23 @@ const Contact = () => {
         <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
             <div className="pb-4">
                 <p className="text-4xl font-bold inline border-b-4 rounded border-gray-500">Contact</p>
-                <p className="py-6">Want to get in contact with me? Submit the form down below!</p>
+                <p className="py-6">Don't hesitate to shoot me a message!</p>
             </div>
 
             <div className="flex justify-center items-center">
                 <form action="https://getform.io/f/e9753a2b-2adc-4042-afa1-9cee8f59b9a0" method="POST" className="flex flex-col w-full">
                     <input type="text" 
                     name="name" 
-                    placeholder="Enter your name"
+                    placeholder="name"
                     className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"/>
 
                     <input type="text" 
                     name="email" 
-                    placeholder="Enter your email"
+                    placeholder="email"
                     className="my-8 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"/>
 
                     <textarea name="message" rows="10" className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-                    placeholder="Enter your message">
+                    placeholder="message">
                     </textarea>
 
                     <button className="text-white bg-gradient-to-r from-cyan-600 to-blue-500 px-6 py-3 my-4 mx-auto flex items-center rounded-md hover:scale-105 duration-300">Submit</button>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+// import React from 'react'
+// import { FaArrowAltCircleDown } from "react-icons/fa";
+// import { Link } from 'react-scroll';
+
+// const Home = () => {
+//   return (
+//     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-900">
+//         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+//             <div className="flex flex-col justify-center h-full">
+//                 {/* Mobile/small is 4xl and desktop/bigger is 7xl */}
+//                 <h2 className="text-4xl sm:text-7xl text-white text-center sm:text-left">Hey, I'm Cameron.</h2>
+
+//                 <p className="text-gray-500 py-4 sm:text-xl">
+//                     I am a <span className="font-bold hover:text-gray-400">Computer Science</span> student pursuing <span className="font-bold hover:text-gray-400">full-stack </span> web development. 
+//                 </p>
+
+//                 <div className="flex justify-center sm:justify-start">
+//                     <Link
+//                         to="contact"
+//                         smooth
+//                         duration={500}
+//                         className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-l from-cyan-600 to-blue-500 cursor-pointer"
+//                     >
+//                         Contact me
+//                         <FaArrowAltCircleDown size={20} className="ml-1" />
+//                     </Link>
+//                     </div>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Home
+
+import React, { useEffect } from 'react';
 import { FaArrowAltCircleDown } from "react-icons/fa";
 import { Link } from 'react-scroll';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 3000, // Animation duration in milliseconds
+    });
+  }, []);
+
   return (
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-900">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-            <div className="flex flex-col justify-center h-full">
+            <div className="flex flex-col justify-center h-full" data-aos="fade-in">
                 {/* Mobile/small is 4xl and desktop/bigger is 7xl */}
                 <h2 className="text-4xl sm:text-7xl text-white text-center sm:text-left">Hey, I'm Cameron.</h2>
 
@@ -24,11 +67,11 @@ const Home = () => {
                         Contact me
                         <FaArrowAltCircleDown size={20} className="ml-1" />
                     </Link>
-                    </div>
+                </div>
             </div>
         </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
